71b9a96e900902d9a64a3892fda7c95c865c64d4
PatrickSouza26 <patrick.souza@residente.serratec.org>
2026-06-19 12:44:05 -0300

    feat: adiciona mapa e input para descrição de obstáculo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
+import { Form } from './src/pages/Form';
 import { Routers } from './src/router';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-      <NavigationContainer>
-        <Routers />
-      </NavigationContainer>
+      // <NavigationContainer>
+      //   <Routers />
+      // </NavigationContainer>
+      <Form></Form>
   );
 }
 
